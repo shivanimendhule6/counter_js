fad126bf7850d5d6261248798d8469a3642feaf0
shivanimendhule6 <shivanimendhule6@gmail.com>
2024-06-23 21:56:42 +0530

    Added reset button and alert

--- Counter-js/index.js
+++ Counter-js/index.js
@@ -5,13 +5,24 @@ function updateCountDisplay() {
 }
 
 function handelIncrement() {
-  count++;
-  updateCountDisplay();
+  if (count < 10) {
+    count++;
+    updateCountDisplay();
+  } else if (count === 10) {
+    alert("Counter has reached 10!");
+  }
 }
 
 function handelDecrement() {
   if (count > 0) {
     count--;
     updateCountDisplay();
+  } else if (count === 0) {
+    alert("Counter has reached 0!");
   }
+}
+
+function ResetToDefault() {
+  count = 0;
+  updateCountDisplay();
 }
